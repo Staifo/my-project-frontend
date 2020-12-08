3b2f7b71b6eaa00bad79d1a35fcfd9f54b7db5bb
Ben Paget <ben.paget@live.fr>
2020-12-08 22:52:52 +0100

    Fix File Upload

--- src/info.js
+++ src/info.js
@@ -9,11 +9,8 @@ import MyComponent from "./searchCode";
 import AnimatedMulti from "./searchCode";
 import AnimatedMultiSkills from "./searchSkills";
 import WebcamStreamCapture from "./cam";
-import async from "async";
-import axios from "axios";
 import FileUpload from "./fileUpload";
-// import FileUploadNew from './fileUpload'
-import CV from "./cv";
+import { useHistory } from "react-router-dom"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,10 +24,22 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Info = () => {
+  const history = useHistory()
   const classes = useStyles();
   const [camDis, setCamDis] = useState(null);
   const [userCreated, setUserCreated] = useState(false);
   const [createCard, setCreateCard] = useState(null);
+  const [videoUploaded, setVideoUploaded] = useState(false)
+  const [fileUploaded, setFileUploaded] = useState({
+    profile_pic: false,
+    CV: false
+  })
+
+  useEffect(() => {
+    if (fileUploaded.profile_pic && fileUploaded.CV) {
+      history.push(`/singleUser/${userCreated._id}`)
+    }
+  }, [fileUploaded])
 
   const camOn = useRef(null);
   const play = useRef(null);
@@ -55,102 +64,17 @@ const Info = () => {
 
   function handleOnClick(e) {
     e.preventDefault();
-    console.log('success')
-    // let payload = form;
-    // console.log(payload);
-    // let data = new FormData();
-    // data.append("json", JSON.stringify(payload));
+    console.log("success");
     fetch("http://localhost:3000/singleuser", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(form),
     })
-      .then( (res) => res.json())
+      .then((res) => res.json())
       .then((data) => setUserCreated(data))
-      .catch(e => console.log(e.message))
-    
-
-    //   setForm(e.target)
-    //   // axios.post('http://localhost:3000/singleUser', form).then(function(req, res) {
-    //   //   console.log(res)
-    //   // })
-    //   e.preventDefault()
-    //   // useEffect( ()=>{
-    //   //   const fetchData = async ()=>{
-    //   //     const config = {
-    //   //     method: 'post',
-    //   //     url: 'http://localhost:3000/singleUsers',
-    //   //     data: { 'job_title': `${form}` ,
-    //   //     'first_name': `${form}` ,
-    //   //     'last_name': `${form}` ,
-    //   //     'email': `${form}` ,
-    //   //     'dob': `${form}` ,
-    //   //     'street':`${form}` ,
-    //   //     'city':`${form}` ,
-    //   //     'postal_code':`${form}` ,
-    //   //     'province':`${form}` ,
-    //   //     'country':`${form}` ,
-    //   //     'github_url':`${form}` ,
-    //   //     'portfolio_url':`${form}` ,
-    //   //     'linkedin_url':`${form}` ,
-    //   //     'xing_url': `${form}` ,
-    //   //     'twitter':`${form}`
-    //   //   }
-    //   //     }
-    //   //   await axios(config)
-    //   //   }
-    //   //   fetchData()
-
-    //   // },[])
-
-    // // try {
-    // //     let result =await fetch('http://localhost:3000/singleUser', {
-    // //       method: 'post',
-    // //       headers: {
-    // //         'Accept': 'application/json',
-    // //         'Content-type': 'application/json',
-    // //       },
-    // //       body: JSON.stringify({
-    // //         first_name: 'hallo'
-
-    // //       })
-    // //     })
-    // //     console.log(result)
-    // //   }catch(e) {
-    // //     console.log(e)
-    // //   }
-
-    //  }
-
-    //  useEffect( ()=>{
-    //      const fetchData = async ()=>{
-    //        const config = {
-    //        method: 'post',
-    //        url: 'http://localhost:3000/singleUsers',
-    //        data: { 'job_title': `${form}` ,
-    //        'first_name': `${form}` ,
-    //        'last_name': `${form}` ,
-    //        'email': `${form}` ,
-    //        'dob': `${form}` ,
-    //        'street':`${form}` ,
-    //        'city':`${form}` ,
-    //        'postal_code':`${form}` ,
-    //        'province':`${form}` ,
-    //        'country':`${form}` ,
-    //        'github_url':`${form}` ,
-    //        'portfolio_url':`${form}` ,
-    //        'linkedin_url':`${form}` ,
-    //        'xing_url': `${form}` ,
-    //        'twitter':`${form}`
-    //      }
-    //        }
-    //      await axios(config)
-    //      }
-    //      fetchData()
-
-    //    },[form])
+      .catch((e) => console.log(e.message));
   }
 
   return (
@@ -161,151 +85,138 @@ const Info = () => {
             className={classes.paper}
             style={{ display: "flex", justifyContent: "space-between" }}
           >
-            {!userCreated && <form
-              // action="http://localhost:3000/singleuser"
-              // method="POST"
-              style={{
-                display: "flex",
-                width: "100%",
-                height: "100%",
-                justifyContent: "space-between",
-              }}
-              onChange={handleOnChange}
-              // enctype="multipart/form-data"
-            >
-              <div
+            {!userCreated && (
+              <form
+                // action="http://localhost:3000/singleuser"
+                // method="POST"
                 style={{
                   display: "flex",
-                  flexDirection: "column",
-                  marginLeft: "2%",
-                  width: "40%",
+                  width: "100%",
+                  height: "100%",
+                  justifyContent: "space-between",
                 }}
+                onChange={handleOnChange}
+                // enctype="multipart/form-data"
               >
-                <div>Job title</div>
-                <input type="text" name="job_title" placeholder="" />
-
-                <div>First Name</div>
-                <input type="text" name="first_name" />
-
-                <div>Last Name</div>
-                <input type="text" name="last_name" />
-
-                <div>Date of Birth</div>
-                <input
-                  name="dob"
-                  type="date"
-                  id="birthday"
-                  style={{ width: "100%" }}
-                />
-
-                <div>Email</div>
-                <input type="email" name="email" />
-
-                <div>Password</div>
-                <input type="password" name="password" />
-
-                <div>Street</div>
-                <input type="text" name="street" />
-
-                <div>City</div>
-                <input type="text" name="city" />
-
-                <div>State/Province</div>
-                <input type="text" name="province" />
-
-                <div>Zip/Postal Code</div>
-                <input type="text" name="postal_code" />
-
-                <div>Country</div>
-                <input type="text" name="country" />
-
-                <div>My Portfolio Link</div>
-                <input type="text" name="portfolio_url" />
-
-                <div>GitHub Link</div>
-                <input type="text" name="github_url" />
-
-                <div>Twitter</div>
-                <input type="text" name="twitter" />
-
-                <div>LinkedIn Link</div>
-                <input type="text" name="linkedin_url" />
-
-                <div>Xing Link</div>
-                <input type="text" name="xing_url" />
-
-                <div>Upload Profile Pic</div>
-                <div>
-                  <FileUpload />
-                </div>
-
-                <div>Upload CV</div>
-                <div>
-                  <CV />
-                </div>
-                {/* <Button
-                  // type="submit"
-                  variant="contained"
-                  color="primary"
-                  style={{ marginRight: "2%", marginTop: "4%", width: "100%" }}
-                  onClick={handleOnClick}
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginLeft: "2%",
+                    width: "40%",
+                  }}
                 >
-                  Send Data
-                </Button> */}
-              </div>
-             
-             
+                  <div>Job title</div>
+                  <input type="text" name="job_title" placeholder="" />
 
-              <div
-                style={{
-                  marginRight: "2%",
-                  width: "38%",
-                  marginLeft: "4%",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <div style={{ display: "flex", width: "100%", height: "100%" }}>
-                  <div style={{ width: "46%", height: "100%" }}>
-                    <div style={{ height: "80px", textAlign: "center" }}>
-                      Coding Skills and Coding Related Skills
-                    </div>
-                    <TextareaAutosize
-                      aria-label="minimum height"
-                      rowsMin={16}
-                      placeholder="Your skills here..."
-                      style={{ width: "100%", height: "80%" }}
-                      name="coding_skills"
-                    />
-                    <br />
-                    {/* <AnimatedMulti /> */}
-                  </div>
-                  <br />
+                  <div>First Name</div>
+                  <input type="text" name="first_name" />
+
+                  <div>Last Name</div>
+                  <input type="text" name="last_name" />
+
+                  <div>Date of Birth</div>
+                  <input
+                    name="dob"
+                    type="date"
+                    id="birthday"
+                    style={{ width: "100%" }}
+                  />
+
+                  <div>Email</div>
+                  <input type="email" name="email" />
+
+                  <div>Password</div>
+                  <input type="password" name="password" />
+
+                  <div>Street</div>
+                  <input type="text" name="street" />
+
+                  <div>City</div>
+                  <input type="text" name="city" />
+
+                  <div>State/Province</div>
+                  <input type="text" name="province" />
+
+                  <div>Zip/Postal Code</div>
+                  <input type="text" name="postal_code" />
+
+                  <div>Country</div>
+                  <input type="text" name="country" />
+
+                  <div>My Portfolio Link</div>
+                  <input type="text" name="portfolio_url" />
+
+                  <div>GitHub Link</div>
+                  <input type="text" name="github_url" />
+
+                  <div>Twitter</div>
+                  <input type="text" name="twitter" />
+
+                  <div>LinkedIn Link</div>
+                  <input type="text" name="linkedin_url" />
+
+                  <div>Xing Link</div>
+                  <input type="text" name="xing_url" />
+                </div>
+
+                <div
+                  style={{
+                    marginRight: "2%",
+                    width: "38%",
+                    marginLeft: "4%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
                   <div
-                    style={{ width: "46%", height: "100%", marginLeft: "10%" }}
+                    style={{ display: "flex", width: "100%", height: "100%" }}
                   >
+                    <div style={{ width: "46%", height: "100%" }}>
+                      <div style={{ height: "80px", textAlign: "center" }}>
+                        Coding Skills and Coding Related Skills
+                      </div>
+                      <TextareaAutosize
+                        aria-label="minimum height"
+                        rowsMin={16}
+                        placeholder="Your skills here..."
+                        style={{ width: "100%", height: "80%" }}
+                        name="coding_skills"
+                      />
+                      <br />
+                      {/* <AnimatedMulti /> */}
+                    </div>
+                    <br />
                     <div
                       style={{
-                        width: "100%",
-                        textAlign: "center",
-                        height: "80px",
+                        width: "46%",
+                        height: "100%",
+                        marginLeft: "10%",
                       }}
                     >
-                      Transferrable Skills and other Soft & Hard Skills (Scale 1(bad) -10(very good)) or write a short text
+                      <div
+                        style={{
+                          width: "100%",
+                          textAlign: "center",
+                          height: "80px",
+                        }}
+                      >
+                        Transferrable Skills and other Soft & Hard Skills (Scale
+                        1(bad) -10(very good)) or write a short text
+                      </div>
+                      <TextareaAutosize
+                        aria-label="minimum height"
+                        rowsMin={16}
+                        placeholder="Your skills here..."
+                        style={{ width: "100%", height: "80%" }}
+                        name="transfer_skills"
+                        placeholder="Your skills or pitch here: for example: Communicational Skills: 10, Adaptability: 10 ..."
+                      />
+                      <br />
+                      {/* <AnimatedMultiSkills /> */}
                     </div>
-                    <TextareaAutosize
-                      aria-label="minimum height"
-                      rowsMin={16}
-                      placeholder="Your skills here..."
-                      style={{ width: "100%", height: "80%" }}
-                      name="transfer_skills"
-                      placeholder = 'Your skills or pitch here: for example: Communicational Skills: 10, Adaptability: 10 ...'
-                    />
-                    <br />
-                    {/* <AnimatedMultiSkills /> */}
                   </div>
-                </div>
-                {/* <Button
+                  {/* <Button
                   type="submit"
                   variant="contained"
                   color="primary"
@@ -314,69 +225,98 @@ const Info = () => {
                 >
                   Send Data
                 </Button> */}
-                <Button
-                  // type="submit"
-                  variant="contained"
-                  color="primary"
-                  style={{ marginRight: "2%", marginTop: "4%", width: "100%" }}
-                  onClick={handleOnClick}
-                >
-                  Send Data
-                </Button>
-              </div>
+                  <Button
+                    // type="submit"
+                    variant="contained"
+                    color="primary"
+                    style={{
+                      marginRight: "2%",
+                      marginTop: "4%",
+                      width: "100%",
+                    }}
+                    onClick={handleOnClick}
+                  >
+                    Send Data
+                  </Button>
+                </div>
 
-              {/* <AnimatedMulti /><br/> */}
-              {/* <AnimatedMultiSkills /> */}
-            </form>}
+                {/* <AnimatedMulti /><br/> */}
+                {/* <AnimatedMultiSkills /> */}
+              </form>
+            )}
           </Paper>
-          {userCreated && <Grid item xs={12}>
-          <div style={{display: 'flex', width: '100%', justifyContent: 'center'}}>
-          <div
+          {userCreated && !videoUploaded && (
+            <Grid item xs={12}>
+              <div
                 style={{
-                  width: "40%",
                   display: "flex",
-                  height: '600px'
+                  width: "100%",
+                  justifyContent: "center",
                 }}
               >
                 <div
-                  ref={play}
                   style={{
+                    width: "40%",
                     display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    position: "relative",
-                    width: "100%",
-
-                    marginLeft: "10%",
+                    height: "600px",
                   }}
                 >
-                  <div style={{ zIndex: "0" }}>
-                    <PlayCircleOutlineIcon
-                      onClick={handleClick}
-                      color="primary"
-                      style={{ fontSize: "20em" }}
-                    />
-                    <span>Click here and start recording your video</span>
-                  </div>
                   <div
-                    ref={camOn}
+                    ref={play}
                     style={{
-                      position: "absolute",
-                      zIndex: "10",
-                      textAlign: "center",
-                      marginRight: "10em",
-                      visibility: "hidden",
-                      zIndex: "10",
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      position: "relative",
+                      width: "100%",
+
+                      marginLeft: "10%",
                     }}
                   >
-                    <WebcamStreamCapture name="video" userId={userCreated._id} />
+                    <div style={{ zIndex: "0" }}>
+                      <PlayCircleOutlineIcon
+                        onClick={handleClick}
+                        color="primary"
+                        style={{ fontSize: "20em" }}
+                      />
+                      <span>Click here and start recording your video</span>
+                    </div>
+                    <div
+                      ref={camOn}
+                      style={{
+                        position: "absolute",
+                        zIndex: "10",
+                        textAlign: "center",
+                        marginRight: "10em",
+                        visibility: "hidden",
+                        zIndex: "10",
+                      }}
+                    >
+                      <WebcamStreamCapture
+                        name="video"
+                        userId={userCreated._id}
+                        setVideoUploaded={setVideoUploaded}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-              </div>
-          </Grid>}
+            </Grid>
+          )}
+          {userCreated && videoUploaded && (
+            <>
+            <div>Upload Profile Pic</div>
+                <div>
+                  <FileUpload userCreated={userCreated} type="profile_pic" setFileUploaded={setFileUploaded}/>
+                </div>
+
+                <div>Upload CV</div>
+                <div>
+                <FileUpload userCreated={userCreated} type="CV" setFileUploaded={setFileUploaded}/>
+                </div>
+            </>
+          )}
         </Grid>
-       
       </Grid>
     </div>
   );
